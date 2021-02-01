pianoboycaleb/ghoulcopy

Use BUGFIX for some inline fixes

src/battle_factory_screen.c
@@ -3905,7 +3905,11 @@ static void Swap_ShowSummaryMonSprite(void)
     personality = GetMonData(mon, MON_DATA_PERSONALITY, NULL);
     otId = GetMonData(mon, MON_DATA_OT_ID, NULL);
 
-    sFactorySwapScreen->unk2C.field0 = CreateMonPicSprite_HandleDeoxys(species, personality, otId, TRUE, 88, 32, 15, 0xFFFF); // BUG: otId and personality should be switched.
+    #ifdef BUGFIX
+    sFactorySwapScreen->unk2C.field0 = CreateMonPicSprite_HandleDeoxys(species, otId, personality, TRUE, 88, 32, 15, 0xFFFF);
+    #else
+    sFactorySwapScreen->unk2C.field0 = CreateMonPicSprite_HandleDeoxys(species, personality, otId, TRUE, 88, 32, 15, 0xFFFF);
+    #endif
     gSprites[sFactorySwapScreen->unk2C.field0].centerToCornerVecX = 0;
     gSprites[sFactorySwapScreen->unk2C.field0].centerToCornerVecY = 0;
 

Fatih120/thpuppetplay2

src/battle_factory_screen.c
@@ -3905,7 +3905,11 @@ static void Swap_ShowSummaryMonSprite(void)
     personality = GetMonData(mon, MON_DATA_PERSONALITY, NULL);
     otId = GetMonData(mon, MON_DATA_OT_ID, NULL);
 
-    sFactorySwapScreen->unk2C.field0 = CreateMonPicSprite_HandleGomaseki(species, personality, otId, TRUE, 88, 32, 15, 0xFFFF); // BUG: otId and personality should be switched.
+    #ifdef BUGFIX
+    sFactorySwapScreen->unk2C.field0 = CreateMonPicSprite_HandleGomaseki(species, otId, personality, TRUE, 88, 32, 15, 0xFFFF);
+    #else
+    sFactorySwapScreen->unk2C.field0 = CreateMonPicSprite_HandleGomaseki(species, personality, otId, TRUE, 88, 32, 15, 0xFFFF);
+    #endif
     gSprites[sFactorySwapScreen->unk2C.field0].centerToCornerVecX = 0;
     gSprites[sFactorySwapScreen->unk2C.field0].centerToCornerVecY = 0;
 